536807f9fcfec6d31f9ca0a4d3aa9e6f0176d375
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-14 12:34:45 -0400

    add lastet recipe and favorite recipes to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import Recipe from '../../components/Recipe/Recipe';
 
-const HomePage = () => {
+const HomePage = ({ latestRecipe, favoriteRecipes, handleFavorites, handleCurrentRecipe, currentRecipe }) => {
+  
+
   return (
-    <div>HomePage</div>
+    <main>
+      {/* latest recipe */}
+      <div className='latestRecipe' onClick={() => handleCurrentRecipe(latestRecipe)}>
+        {latestRecipe}
+      </div>
+
+      {/* favorite recipes */}
+      <div className='favorite-carousel'>
+        {
+          favoriteRecipes.map((recipe) => (
+            <Recipe 
+              key={recipe.id} 
+              recipe={recipe}
+              handleFavorites={handleFavorites}
+              handleCurrentRecipe={handleCurrentRecipe}
+               />
+          ))
+        }
+      </div>
+    </main>
   )
 }
 
