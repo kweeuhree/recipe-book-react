04a587a8d6a9e9e975cae6ec2645882cde03b6bf
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-14 18:36:25 -0400

    add two views of Recipe

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+// import components
 import ImageContainer from '../ImageContainer/ImageContainer';
+// import styles
+import './RecipeStyle.css';
 
 const Recipe = ({ recipe, handleFavorites, handleCurrentRecipe }) => {
-  
+
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const recipePreview = () => {
+    return (
+      <>
+        <h3>{recipe.title}</h3>
+        <div>{recipe.description}</div>
+        <div className='servings'>Servings: <span>{recipe.servings}</span></div>
+        <div>{recipe.date_isLiked}</div>
+        <ImageContainer src={recipe.image} alt={recipe.title} />
+      </>
+    )
+  }
+
+  const recipeFull = () => {
+    return (
+      <>
+        <h2>{recipe.title}</h2>
+        <div>{recipe.description}</div>
+        <div>{recipe.ingredients}</div>
+        <div>{recipe.instructions}</div>
+        <div>{recipe.servings}</div>
+        <div>{recipe.date_created}</div>
+        <div>{recipe.date_updated}</div>
+        <div>{recipe.date_isLiked}</div>
+        <ImageContainer src={recipe.image} alt={recipe.title} />
+      </>
+    )
+  }
+
   return (
-    <div className='recipe'>
-      <h2>{recipe.title}</h2>
-      <div>{recipe.description}</div>
-      <div>{recipe.ingredients}</div>
-      <div>{recipe.instructions}</div>
-      <div>{recipe.servings}</div>
-      <div>{recipe.date_created}</div>
-      <div>{recipe.date_updated}</div>
-      <div>{recipe.date_isLiked}</div>
-      <ImageContainer src={recipe.image} alt={recipe.title} />
-
-
+    <div className='recipe' onClick={handleCurrentRecipe}>
+        { pathname === '/recipe/:id' ? recipeFull() : recipePreview() }
     </div>
   )
 }
