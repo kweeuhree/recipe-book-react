b91167969df8563c0f0765a3e89b725b49e35874
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-14 12:21:11 -0400

    add fetching, like-unlike logic for Recipe

---
+++ src/utils/fetchRecipes.js
@@ -0,0 +1,54 @@
+export const fetchRecipes = async () => {
+   try {
+    const response = await fetch('http://localhost:8000/api/recipes/', {
+        method: 'GET'
+    })
+
+    const recipeData = await response.json();
+    console.log('received recipe data inside fetchRecipes', recipeData);
+    
+    return recipeData;
+
+   } catch(error) {
+    
+    console.error('Error fetching recipes:', error);
+    throw error;
+   }
+}
+
+export const unlikeFavoriteRecipe = async (recipeId) => {
+    try {
+      const response = await fetch(`/api/recipes/${recipeId}/unlike/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Failed to unlike a recipe');
+      }
+
+    } catch (error) {
+      console.error('Error unliking a recipe:', error);
+      throw error;
+    }
+  };
+
+export const likeARecipe = async (recipeId) => {
+    try {
+      const response = await fetch(`/api/recipes/${recipeId}/like/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+      });
+      if (!response.ok) {
+        throw new Error('Failed to like a recipe');
+      }
+
+    } catch (error) {
+      console.error('Error liking a recipe:', error);
+      throw error;
+    }
+  };
