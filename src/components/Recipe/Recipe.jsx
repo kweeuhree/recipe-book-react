da0bfdcb8f87cfb7b1edc7c2a3b6bc3284d72eb7
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-14 16:48:40 -0400

    initial Recipe component display

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import ImageContainer from '../ImageContainer/ImageContainer';
 
 const Recipe = ({ recipe, handleFavorites, handleCurrentRecipe }) => {
+  
   return (
     <div className='recipe'>
-      Recipe
+      <h2>{recipe.title}</h2>
+      <div>{recipe.description}</div>
+      <div>{recipe.ingredients}</div>
+      <div>{recipe.instructions}</div>
+      <div>{recipe.servings}</div>
+      <div>{recipe.date_created}</div>
+      <div>{recipe.date_updated}</div>
+      <div>{recipe.date_isLiked}</div>
+      <ImageContainer src={recipe.image} alt={recipe.title} />
+
+
     </div>
   )
 }
