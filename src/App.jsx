a9e65d34efa70210b64a56e9c86b045d0ff27df2
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-14 11:00:25 -0400

    fill routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import route, routes, and navigate component
 import { Route, Routes, Navigate } from 'react-router-dom';
+// import fetching logic
+import fetchRecipes from './utils/fetchRecipes';
 // import components
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
@@ -13,7 +15,19 @@ import SpecificRecipePage from './pages/SpecificRecipePage/SpecificRecipePage';
 import './App.css'
 
 function App() {
+  const [ allRecipes, setAllRecipes ] = useState([]);
+  const [ favoriteRecipes, setFavoriteRecipes ] = useState([]);
+  const [ latestRecipe, setLatestRecipe ] = useState(null);
 
+  useEffect(()=> {
+      const fetchAllRecipes = async() => {
+        const recipesData = await fetchRecipes();
+        setAllRecipes((prev) => ([...prev, recipesData]));
+        setLatestRecipe(recipesData[0])
+      }
+
+      fetchAllRecipes();
+  }, [])
 
   return (
     <>
@@ -22,15 +36,15 @@ function App() {
 
     {/* routes */}
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<Navigate to="/home" />} /> 
         {/* home page */}
         <Route path="/home" element={<HomePage />} />
         {/* all recipes */}
-        <Route path="/all" element={<AllRecipesPage />} />
+        <Route path="/all" element={<AllRecipesPage recipes={<allRecipes />} />} />
         {/* latest recipe */}
-        <Route path={`/recipe/:id`} element={<SpecificRecipePage />} />
+        <Route path={`/recipe`} element={<SpecificRecipePage recipe={latestRecipe} />} />
         {/* favorite recipes */}
-        <Route path="/favorites" element={<FavoriteRecipesPage />} />
+        <Route path="/favorites" element={<FavoriteRecipesPage recipes={favoriteRecipes} />} />
       </Routes>
 
     {/* footer */}
