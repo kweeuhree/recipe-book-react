6909fd9849c45a9268e0e284ec3a8e219cc3aa16
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-15 11:47:37 -0400

    remove useParams

--- src/pages/SpecificRecipePage/SpecificRecipePage.jsx
+++ src/pages/SpecificRecipePage/SpecificRecipePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
 import Recipe from '../../components/Recipe/Recipe';
 
 const SpecificRecipePage = ({ handleFavorites, favoriteRecipes, handleCurrentRecipe, currentRecipe }) => {  
