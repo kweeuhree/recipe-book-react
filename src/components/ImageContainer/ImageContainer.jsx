8e670ed3d81cf9e5bb65833ca8ec1903105efd2e
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-16 12:50:20 -0400

    add default recipe image

--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import styles
 import './ImageContainerStyle.css';
+import defaultImage from '../../assets/default-image.png';
 
 const ImageContainer = ({ src, alt }) => {
   return (
     <div className='image-container'>
-        <img src={src} alt={alt} />
+        <img src={src ? src : defaultImage} alt={alt ? alt : 'Frying pan with a spatula'} />
     </div>
   )
 }
