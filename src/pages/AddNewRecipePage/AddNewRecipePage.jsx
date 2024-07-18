92229dede06525fe2bd0307b52052754faa3b732
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-18 08:21:56 -0400

    add comments to AddNewRecipePage

--- src/pages/AddNewRecipePage/AddNewRecipePage.jsx
+++ src/pages/AddNewRecipePage/AddNewRecipePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+//  import navigate object from react router dom
 import { useNavigate } from 'react-router-dom';
 // import fetching logic 
 import { postARecipe } from '../../utils/fetchRecipes';
@@ -7,19 +8,21 @@ import CreateForm from '../../components/Form/CreateForm';
 
 
 const AddNewRecipePage = ({ updateAllRecipes, handleCurrentRecipe, handleLatestRecipe }) => {
-
+  // initialize state to store new recipe status message
   const [ message, setMessage ] = useState('');
   const navigate = useNavigate();
   
-
+// handle posting a new recipe
   const postRecipe = async (recipeData) => {
     try {
+      // send database request to post a new recipe
         const newRecipe = await postARecipe(recipeData); 
-
+        // update status message
         updateMessage('New Recipe saved!');
+        // update recipe states
         handleLatestRecipe(newRecipe);
         updateAllRecipes(newRecipe);
-
+        // navigate to all recipes page after a timeout
         setTimeout(() => {
           navigate('/recipes/');
         }, 500);
@@ -27,18 +30,19 @@ const AddNewRecipePage = ({ updateAllRecipes, handleCurrentRecipe, handleLatestR
 
     } catch(error) {
       console.log(error);
-      
+      // update status message
       updateMessage('New Recipe is not saved.');
       throw new Error('Failed posting the recipe.');
       
     } finally {
-
+      // after a timeout, reset status message regardless of outcome
       setTimeout(() => {
         setMessage('');
       }, 3000);
     }
   }
 
+  // handle update status message
   const updateMessage = (newMessage) => {
     setMessage(newMessage);
   }
@@ -47,6 +51,7 @@ const AddNewRecipePage = ({ updateAllRecipes, handleCurrentRecipe, handleLatestR
     <div className='create-from-container'>
       {/* form */}
       <CreateForm post={postRecipe} />
+      {/* status message */}
       <p>{message}</p>
     </div>
   )
