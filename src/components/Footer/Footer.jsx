0412804a4fc84771f5632557946a79642564c864
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-18 08:37:45 -0400

    add comments in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
-
+    // get current day and year
     const today = new Date();
     const year = today.getFullYear();
+
     const author = 'Veronika Kolesnikova';
 
   return (
+    // display footer
     <footer>&copy; {year}, {author}</footer>
   )
 }
