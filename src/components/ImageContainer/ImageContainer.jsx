a6b5b1128116dc4b2cb0b98de10f6502b86adb54
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-18 08:36:54 -0400

    add comments in Imagecontainer

--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import styles
 import './ImageContainerStyle.css';
+// import default image
 import defaultImage from '../../assets/default-image.png';
 
 const ImageContainer = ({ src, alt }) => {
   return (
     <div className='image-container'>
+      {/* conditionally display image */}
         <img src={src ? src : defaultImage} alt={alt ? alt : 'Frying pan with a spatula'} />
     </div>
   )
