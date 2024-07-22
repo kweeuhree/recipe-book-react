0c8415691e4d1e898f7eee1d390c41a73137d2dd
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-22 17:14:28 -0400

    comment out state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react';
+// import context
+import { RecipeContext } from './context/RecipeContext';
 // import route, routes, and navigate component
 import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 // import fetching logic
-import { fetchRecipes, unlikeFavoriteRecipe, likeARecipe } from './utils/fetchRecipes';
+// import { fetchRecipes, unlikeFavoriteRecipe, likeARecipe } from './utils/fetchRecipes';
 // import components
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
@@ -16,125 +18,117 @@ import AddNewRecipePage from './pages/AddNewRecipePage/AddNewRecipePage';
 import './App.css'
 
 function App() {
-  // initialize state to store an array of all recipes
-  const [ allRecipes, setAllRecipes ] = useState([]);
-  // initialize state to store an array of favorite recipes
-  const [ favoriteRecipes, setFavoriteRecipes ] = useState([]);
-  // intiialize state to store current recipe
-  const [ currentRecipe, setCurrentRecipe ] = useState(null);
-  // initialize state to store last added recipe
-  const [ latestRecipe, setLatestRecipe ] = useState(null);
 
   const navigate = useNavigate();
 
-  // fetch all recipes
-  useEffect(()=> {
-      const fetchAllRecipes = async () => {
-        const recipesData = await fetchRecipes();
-        // latest recipe is the first one in the array
-        const latestRecipe = recipesData[0];
-        //update state based on received data
-        setAllRecipes(recipesData);
-        handleLatestRecipe(latestRecipe);
-        updateFavoriteRecipes(recipesData);
-      }
+//   // fetch all recipes
+//   useEffect(()=> {
+//       const fetchAllRecipes = async () => {
+//         const recipesData = await fetchRecipes();
+//         // latest recipe is the first one in the array
+//         const latestRecipe = recipesData[0];
+//         //update state based on received data
+//         setAllRecipes(recipesData);
+//         handleLatestRecipe(latestRecipe);
+//         updateFavoriteRecipes(recipesData);
+//       }
       
-      fetchAllRecipes();
-  }, []);
+//       fetchAllRecipes();
+//   }, []);
 
- // update all recipes
-  const updateAllRecipes = (recipe) => {
+//  // update all recipes
+//   const updateAllRecipes = (recipe) => {
     
-    // if recipe id matches an already existing one
-    const found = allRecipes.find(r => r.id === recipe.id);
-    if(found) {
-      setAllRecipes((prevAllRecipes) => {
-        return prevAllRecipes.map((r) => {
-          // update recipe with the matching id
-          if (r.id === recipe.id) {
-            return {
-              ...r,
-              title: recipe.title || r.title,
-              description: recipe.description || r.description,
-              ingredients: recipe.ingredients || r.ingredients,
-              instructions: recipe.instructions || r.instructions,
-              servings: recipe.servings || r.servings
-            };
-          }
-          return r; // return recipes that weren't updated
-        });
-      });
-    } else {
-      // if id didnt match, add recipe to array
-      setAllRecipes((prevAllRecipes) => ([recipe, ...prevAllRecipes]));
-    }
-  }
+//     // if recipe id matches an already existing one
+//     const found = allRecipes.find(r => r.id === recipe.id);
+//     if(found) {
+//       setAllRecipes((prevAllRecipes) => {
+//         return prevAllRecipes.map((r) => {
+//           // update recipe with the matching id
+//           if (r.id === recipe.id) {
+//             return {
+//               ...r,
+//               title: recipe.title || r.title,
+//               description: recipe.description || r.description,
+//               ingredients: recipe.ingredients || r.ingredients,
+//               instructions: recipe.instructions || r.instructions,
+//               servings: recipe.servings || r.servings
+//             };
+//           }
+//           return r; // return recipes that weren't updated
+//         });
+//       });
+//     } else {
+//       // if id didnt match, add recipe to array
+//       setAllRecipes((prevAllRecipes) => ([recipe, ...prevAllRecipes]));
+//     }
+//   }
 
-  // handle updating current recipe
-  const handleCurrentRecipe = (recipe) => {
-    setCurrentRecipe(recipe);
-    console.log(recipe, 'inside handler')
-    // navigate to page
-    navigate(`/recipes/${recipe.id}/`);
-  }
+//   // handle updating current recipe
+//   const handleCurrentRecipe = (recipe) => {
+//     setCurrentRecipe(recipe);
+//     console.log(recipe, 'inside handler')
+//     // navigate to page
+//     navigate(`/recipes/${recipe.id}/`);
+//   }
 
-  // handle initial state of favorite recipes
-  const updateFavoriteRecipes = (recipesData) => {
-    // get array with liked recipes, filter by isLiked
-    const likedRecipes = recipesData?.filter((recipe) => (
-      recipe.isLiked === true
-    ))
-    // set state with new array
-    setFavoriteRecipes(likedRecipes);
-  }
+//   // handle initial state of favorite recipes
+//   const updateFavoriteRecipes = (recipesData) => {
+//     // get array with liked recipes, filter by isLiked
+//     const likedRecipes = recipesData?.filter((recipe) => (
+//       recipe.isLiked === true
+//     ))
+//     // set state with new array
+//     setFavoriteRecipes(likedRecipes);
+//   }
 
-  const filterFavorites = (recipeId) => {
-    setFavoriteRecipes((prev) => 
-      (prev.filter((favRecipe) => favRecipe.id !== recipeId)));
-  }
+//   const filterFavorites = (recipeId) => {
+//     setFavoriteRecipes((prev) => 
+//       (prev.filter((favRecipe) => favRecipe.id !== recipeId)));
+//   }
 
-  // handle adding or removing from favorite recipes
-  const handleFavorites = async (recipeId) => {
-    // check if recipe is in favorite recipes
-    const found = favoriteRecipes.find(
-      favRecipe => favRecipe.id === recipeId
-    );
+//   // handle adding or removing from favorite recipes
+//   const handleFavorites = async (recipeId) => {
+//     // check if recipe is in favorite recipes
+//     const found = favoriteRecipes.find(
+//       favRecipe => favRecipe.id === recipeId
+//     );
 
-    // if recipe is liked, remove from favorites
-    if(found) {
-      filterFavorites(recipeId);
-      // send database request
-      await unlikeFavoriteRecipe(recipeId);
-      // if recipe isnt liked 
-    } else {
-      // add to favorite recipes
-      const newFav = allRecipes.find((r) => r.id === recipeId);
-      setFavoriteRecipes((prev) => ([newFav, ...prev]));
-      // send database request
-      await likeARecipe(recipeId);
-    }
+//     // if recipe is liked, remove from favorites
+//     if(found) {
+//       filterFavorites(recipeId);
+//       // send database request
+//       await unlikeFavoriteRecipe(recipeId);
+//       // if recipe isnt liked 
+//     } else {
+//       // add to favorite recipes
+//       const newFav = allRecipes.find((r) => r.id === recipeId);
+//       setFavoriteRecipes((prev) => ([newFav, ...prev]));
+//       // send database request
+//       await likeARecipe(recipeId);
+//     }
 
-  }
+//   }
 
-  // update latest recipe
-  const handleLatestRecipe = (recipe) => {
-    setLatestRecipe(recipe);
-  }
+//   // update latest recipe
+//   const handleLatestRecipe = (recipe) => {
+//     setLatestRecipe(recipe);
+//   }
 
-  // filter all recipes based on recipeId
-  const handleFilterAllRecipes = (recipeId) => {
+//   // filter all recipes based on recipeId
+//   const handleFilterAllRecipes = (recipeId) => {
 
-    setAllRecipes((prevAllRecipes) => (
-      // filter by id
-      prevAllRecipes.filter((r) => (
-        r.id !== recipeId
-      ))
-    ))
-    // filter favorites
-    filterFavorites(recipeId);
-    // update latest recipe if ids match
-    latestRecipe.id === recipeId && setLatestRecipe(allRecipes[1]);
-  }
+//     setAllRecipes((prevAllRecipes) => (
+//       // filter by id
+//       prevAllRecipes.filter((r) => (
+//         r.id !== recipeId
+//       ))
+//     ))
+//     // filter favorites
+//     filterFavorites(recipeId);
+//     // update latest recipe if ids match
+//     latestRecipe.id === recipeId && setLatestRecipe(allRecipes[1]);
+//   }
 
 
 
