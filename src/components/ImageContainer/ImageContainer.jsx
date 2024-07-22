1ef831e32e7f9130c1792715e0f75800a5dc0d43
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-22 17:46:08 -0400

    update imageContainer to have default value instead of conditional rendering

--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -4,11 +4,11 @@ import './ImageContainerStyle.css';
 // import default image
 import defaultImage from '../../assets/default-image.png';
 
-const ImageContainer = ({ src, alt }) => {
+const ImageContainer = ({ src = defaultImage, alt = 'Frying pan with a spatula' }) => {
   return (
     <div className='image-container'>
       {/* conditionally display image */}
-        <img src={src ? src : defaultImage} alt={alt ? alt : 'Frying pan with a spatula'} />
+        <img src={src} alt={alt} />
     </div>
   )
 }
